3fd819ee2ab4a174f719138469203c6b9727e55c
VolkovaSA <volkova.sa@yahoo.com>
2025-11-10 00:32:56 +0300

    Files updated

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.time.Duration;
+
+public class BaseTest {
+    WebDriver browser;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+
+    @BeforeMethod
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--guest");
+        browser = new ChromeDriver(options);
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        loginPage = new LoginPage(browser);
+        productsPage = new ProductsPage(browser);
+    }
+
+    @AfterMethod
+    public void close() {
+        browser.quit();
+    }
+}
